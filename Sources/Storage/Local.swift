3c1495eb746f58e2dc0848ae411fb86b3b55d478
Tomasz Sądej <tomasz@tbx.pl>
2025-03-11 12:08:48 +0100

    revert(dfbd4d4): disable local storage cleanup for linux

--- Sources/Storage/Local.swift
+++ Sources/Storage/Local.swift
@@ -34,11 +34,6 @@ struct Local: Storage {
     }
 
     func cleanup(_ directory: String, keep: Int) async throws {
-        #if !canImport(Darwin)
-            print("Backups cleanup for local storage is only availabe on macOS.")
-            return
-        #endif
-
         let destination: URL
         if directory.first == "/" {
             destination = URL(fileURLWithPath: directory)
